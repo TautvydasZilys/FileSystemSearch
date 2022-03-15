352fd8a38a7ca9bed62801969d9e0188f6b7e3ad
Tautvydas Žilys <tautvydas.zilys@gmail.com>
2022-03-14 22:51:02 -0700

    Drop support for Windows 7.

--- SearchResultsView/PrecompiledHeader.h
+++ SearchResultsView/PrecompiledHeader.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define NTDDI_VERSION 0x06010000
-#define WINVER 0x0601
-#define _WIN32_WINNT 0x0601
+#define NTDDI_VERSION 0x0A000007 // NTDDI_WIN10_19H1
+#define _WIN32_WINNT 0x0A00 // _WIN32_WINNT_WIN10
+#define WINVER 0x0A00
 
 #include <ShellScalingApi.h>
 #include <Shlobj.h>
