20819c0adad6e9c87f3bdea9674c59faf0b60e24
Tautvydas Žilys <tautvydas.zilys@gmail.com>
2022-03-20 16:27:58 -0700

    Fix thread name.

--- SearchEngine/Source/SearchResultReporter.cpp
+++ SearchEngine/Source/SearchResultReporter.cpp
@@ -48,7 +48,7 @@ double SearchResultReporter::GetTotalSearchTimeInSeconds()
 
 void SearchResultReporter::InitializeSearchResultDispatcherWorkerThread()
 {
-	SetThreadDescription(GetCurrentThread(), L"FSS Search Result Dispatcher Thread");
+	SetThreadDescription(GetCurrentThread(), L"FSS Result Dispatcher Thread");
 
 	DoWork([this](const SearchResultData& searchResult)
 	{
