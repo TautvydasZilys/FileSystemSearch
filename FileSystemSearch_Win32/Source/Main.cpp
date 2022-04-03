a42741a528ad5c5a7da6bb891872d54c2f5d3eba
Tautvydas Žilys <tautvydas.zilys@gmail.com>
2022-04-02 19:24:48 -0700

    Add initial Win32 UI project.

---
+++ FileSystemSearch_Win32/Source/Main.cpp
@@ -0,0 +1,6 @@
+#include "PrecompiledHeader.h"
+
+int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
+{
+    return 0;
+}
---
+++ FileSystemSearch_Win32/Source/PrecompiledHeader.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define NTDDI_VERSION 0x0A000007 // NTDDI_WIN10_19H1
+#define _WIN32_WINNT 0x0A00 // _WIN32_WINNT_WIN10
+#define WINVER 0x0A00
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+#include <Windows.h>
+
+#if _DEBUG
+#define Assert(x) do { if (!(x)) __debugbreak(); } while (false, false)
+#else
+#define Assert(x) do { if (false, false) (void)(x); } while (false, false)
+#endif
